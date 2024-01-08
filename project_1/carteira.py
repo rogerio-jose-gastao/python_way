760ffeb49cc079420c60857663dbcc0cac0e7158
Rogério Gastão <eltrondemais@gmail.com>
2024-01-08 16:29:33 +0100

    Transformar em funcao

--- project_1/carteira.py
+++ project_1/carteira.py
@@ -1,10 +1,13 @@
 # Um programa basico que compara a idade para conduzir
-idade = 20
-possui_carteira = True
 
-if (idade > 18) and possui_carteira:
-    print("Voce pode dirigir.")
-elif possui_carteira:
-    print("Voce apenas possui a carteira. Espere a idade.")
-else:
-    print("Voce não pode dirigir.")
+def idade_conducao(nome, possui_carteira, idade = 20):
+    if (idade > 18) and possui_carteira:
+        print("{nome} pode dirigir.")
+    elif possui_carteira:
+        print("{nome} apenas possui a carteira. Espere a idade.")
+    else:
+        print("{nome} não pode dirigir.")
+
+idade_conducao("Aurora", idade=18)
+idade_conducao("Nando", idade=10)
+idade_conducao("Rin", False)
