fcff8c6d15a47745d1a99110e17bf9b2771c5505
Rogério Gastão <eltrondemais@gmail.com>
2024-01-08 16:33:58 +0100

    acrescentar formato

--- project_1/carteira.py
+++ project_1/carteira.py
@@ -2,11 +2,11 @@
 
 def idade_conducao(nome, possui_carteira=True, idade = 20):
     if (idade > 18) and possui_carteira:
-        print("{nome} pode dirigir.")
+        print(f"{nome} pode dirigir.")
     elif possui_carteira:
-        print("{nome} apenas possui a carteira. Espere a idade.")
+        print(f"{nome} apenas possui a carteira. Espere a idade.")
     else:
-        print("{nome} nao pode dirigir.")
+        print(f"{nome} nao pode dirigir.")
 
 idade_conducao("Aurora",idade=19)
 idade_conducao("Nando", idade=10)
